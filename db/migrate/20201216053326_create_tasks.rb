b051d6e979ffd552d49e028370f93251eb1eeb77
ryancdavey1 <ryancdavey1@gmail.com>
2020-12-15 23:07:31 -0700

    task model

---
+++ db/migrate/20201216053326_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.date :start_date
+      t.boolean :completed_status
+      t.integer :initiative_id
+
+      t.timestamps
+    end
+  end
+end
