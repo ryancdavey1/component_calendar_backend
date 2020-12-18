a3f54531f920df7e4b1e05312a9b079713fe9c87
ryancdavey1 <ryancdavey1@gmail.com>
2020-12-18 13:47:01 -0700

    add index and create routes to tasks controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::TasksController < ApplicationController
-  
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      render json: task, status: :accepted
+    else 
+      render json: {errors: task.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :start_date, :completed_status, :initiative_id)
+  end
 end
